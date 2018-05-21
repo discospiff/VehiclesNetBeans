f9554efbcb0837a7ddb909dd58ed90ac8ef65d6a
jonesbr <brandan.jones@uc.edu>
2018-05-20 20:27:26 -0400

    Start our program

---
+++ src/edu/uc/jonesbr/vehicles/Driver.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.uc.jonesbr.vehicles;
+
+/**
+ * This represents a driver of a vehicle.
+ * @author ucint
+ */
+public class Driver {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        promptUser();
+    }
+    
+    /**
+     * Prompt the user for information about trips and MPG
+     */
+    public static void promptUser() {
+        int mpg = 25;
+        double gallonsOfGas = 10;
+        int distance = 50;
+        
+        double gallonsConsumed = distance / mpg;
+        gallonsOfGas = gallonsOfGas - gallonsConsumed;
+        System.out.println("Gas Consumed: " + gallonsConsumed + " Gallons remaining: " + gallonsOfGas);
+    }
+    
+    
+}
---
+++ src/edu/uc/jonesbr/vehicles/Vehicle.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.uc.jonesbr.vehicles;
+
+/**
+ *
+ * @author ucint
+ */
+public class Vehicle {
+    private double gallonsOfGas;
+    private int milesPerGallon;
+
+    /**
+     * @return the gallonsOfGas
+     */
+    public double getGallonsOfGas() {
+        return gallonsOfGas;
+    }
+
+    /**
+     * @param gallonsOfGas the gallonsOfGas to set
+     */
+    public void setGallonsOfGas(double gallonsOfGas) {
+        this.gallonsOfGas = gallonsOfGas;
+    }
+
+    /**
+     * @return the milesPerGallon
+     */
+    public int getMilesPerGallon() {
+        return milesPerGallon;
+    }
+
+    /**
+     * @param milesPerGallon the milesPerGallon to set
+     */
+    public void setMilesPerGallon(int milesPerGallon) {
+        this.milesPerGallon = milesPerGallon;
+    }
+    
+    
+}
