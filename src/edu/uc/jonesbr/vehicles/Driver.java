97dbae61f38721d02fb34d566b9e208506e03b5f
jonesbr <brandan.jones@uc.edu>
2018-06-21 20:07:02 -0400

    Prompt for multiple pieces of data with a do-while loop.

--- src/edu/uc/jonesbr/vehicles/Driver.java
+++ src/edu/uc/jonesbr/vehicles/Driver.java
@@ -27,51 +27,47 @@ public class Driver {
      */
     public static void promptUser() {
         
-        // create a variable of type Vehicle.
-        Vehicle myVehicle = new Vehicle();
-        // create an object of type Vehicle.  This object is different from myVehicle, and will have its own independent values.
-        Vehicle yourVehicle = new Vehicle();
-        
-        // initialize the values of yourVehicle
-        yourVehicle.setGallonsOfGas(8);
-        yourVehicle.setMilesPerGallon(20);
-        yourVehicle.setOdometer(30000);
-        // print the current state of the vehicle
-        System.out.println("My Vehicle:");
-        System.out.println(myVehicle.toString());
-        
-        System.out.println("Your Vehicle:");
-        System.out.println(yourVehicle.toString());
+        Vehicle myVehicle;
+        int createAnother;
+            
+        do {
+            myVehicle  = new Vehicle();
+            // prompt user
+            String strGallonsOfGas = JOptionPane.showInputDialog("Enter gallons of gas");
 
-        // prompt user
-        String strGallonsOfGas = JOptionPane.showInputDialog("Enter gallons of gas");
-        
-        // convert the String return to a double.
-        double dblGallonsOfGas = Double.parseDouble(strGallonsOfGas);
-        
-        myVehicle.setGallonsOfGas(dblGallonsOfGas);
-        
-        final int intMilesPerGallon = promptForInteger("Enter miles per gallon", 0, 100);
-        
-        myVehicle.setMilesPerGallon(intMilesPerGallon);
-        
-        int intOdometer = promptForInteger("Enter odometer", 0, 1000000);
-        
-        myVehicle.setOdometer(intOdometer);
-        
+            // convert the string return to a double
+            double dblGallonsOfGas = Double.parseDouble(strGallonsOfGas);
+
+            // set the attributes in myVehicle object.
+            myVehicle.setGallonsOfGas(dblGallonsOfGas);
+
+            final int intMilesPerGallon = promptForInteger("Enter miles per gallon", 0, 100);
+
+            myVehicle.setMilesPerGallon(intMilesPerGallon);
+
+            int intOdometer = promptForInteger("Enter odometer", 0, 1000000);
+
+            myVehicle.setOdometer(intOdometer);
+
+            // prompt the user to create another vehicle.
+            createAnother = JOptionPane.showConfirmDialog(null, "Do you want to create another vehicle?", "Create another vehicle?", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+            
+        // if the user chooses yes, prompt for another vehicle.  Otherwise, do not loop again.    
+        } while (createAnother == JOptionPane.YES_OPTION);        
+   
         final String strDistanceTravelled = JOptionPane.showInputDialog("Enter distance to travel");
         final int intDistance = Integer.parseInt(strDistanceTravelled);
 
         // print the current state of the vehicle
-        System.out.println("My Vehicle");
         System.out.println(myVehicle.toString());
         
         // move the vehicle.
         myVehicle.go(intDistance);
         
         // print the current state of the vehicle
-        System.out.println("Your Vehicle:");
-        System.out.println(yourVehicle.toString());
+        System.out.println("My Vehicle");
+        System.out.println(myVehicle.toString());
+        
         
     }
     
