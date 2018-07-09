96192b789f4362f7eef2b7c45a36d7725d5032e7
jonesbr <brandan.jones@uc.edu>
2018-07-09 19:02:29 -0400

    Create Vehicle subclasses This is part of a video series on learning Java, which is available here: https://www.youtube.com/watch?v=v9byUJafktg&list=PL73qvSDlAVVjmeAzZWEjkY73aupv2l2Y7

---
+++ src/edu/uc/jonesbr/vehicles/Cavalier.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.uc.jonesbr.vehicles;
+
+/**
+ *
+ * @author ucint
+ */
+public class Cavalier extends Vehicle {
+    
+    private boolean convertible;
+
+    /**
+     * @return the convertible
+     */
+    public boolean isConvertible() {
+        return convertible;
+    }
+
+    /**
+     * @param convertible the convertible to set
+     */
+    public void setConvertible(boolean convertible) {
+        this.convertible = convertible;
+    }
+    
+}
---
+++ src/edu/uc/jonesbr/vehicles/Prius.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.uc.jonesbr.vehicles;
+
+/**
+ *
+ * @author ucint
+ */
+public class Prius extends Vehicle {
+    private int milliampHours;
+    private int milesPerMah;
+
+    @Override
+    public void go(int distance) {
+        System.out.println("In prius, running in internal combustion mode.");
+        super.go(distance); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+    
+    /**
+     * @return the milliampHours
+     */
+    public int getMilliampHours() {
+        return milliampHours;
+    }
+
+    /**
+     * @param milliampHours the milliampHours to set
+     */
+    public void setMilliampHours(int milliampHours) {
+        this.milliampHours = milliampHours;
+    }
+
+    /**
+     * @return the milesPerMah
+     */
+    public int getMilesPerMah() {
+        return milesPerMah;
+    }
+
+    /**
+     * @param milesPerMah the milesPerMah to set
+     */
+    public void setMilesPerMah(int milesPerMah) {
+        this.milesPerMah = milesPerMah;
+    }
+    
+    
+}
